5d56c7aa2d590cd3b6d47908bee1695acfb0087a
Hunter Perkins <112034278+WanderingKhajiit@users.noreply.github.com>
2022-12-21 13:50:02 -0500

    final commit
    had to search for this

--- src/Layout/Components/AddCard.js
+++ src/Layout/Components/AddCard.js
@@ -1,58 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { createCard, readDeck } from "../../utils/api/index";
+import SharedForm from "./SharedForm"
 
 function AddCard() {
-    const { deckId } = useParams();
-    const history = useHistory();
-    const initialState = {
-        front: "",
-        back: "",
+    const mountedRef = useRef(false);
+    // Default for new card data
+    const initialFormState = {
+      id: '',
+      front: '',
+      back: '',
+      deckId: '',
     };
-
-    const [newCard, setNewCard] = useState(initialState);
-    const [deck, setDeck] = useState({});
-
+    const [deck, setDeck] = useState({
+      name: 'loading...',
+      description: '',
+      cards: [],
+    });
+    const [newCard, setNewCard] = useState(initialFormState);
+    const history = useHistory();
+    const { deckId } = useParams();
+    // effect just for tracking state
     useEffect(() => {
-        async function fetchData() {
-            const abortController = new AbortController();
-            try {
-                const response = await readDeck(deckId, abortController.signal);
-                setDeck(response);
-            } catch (error) {
-                console.error("Something went wrong", error);
-            }
-            return () => {
-                abortController.abort();
-            };
+      mountedRef.current = true;
+      return () => {
+        mountedRef.current = false;
+      };
+    }, []);
+  
+    // Load deck effect
+    useEffect(() => {
+      const abortController = new AbortController();
+      async function loadDeck() {
+        try {
+          const loadedDeck = await readDeck(deckId, abortController.signal);
+          if (mountedRef.current) {
+            setDeck(() => loadedDeck);
+          }
+        } catch (error) {
+          if (error.name !== 'AbortError') {
+            throw error;
+          }
         }
-        fetchData();
+      }
+      loadDeck();
+      return () => {
+        abortController.abort();
+      };
     }, [deckId]);
-
-    function handleChange({ target }) {
-        setNewCard({
-            ...newCard,
-            [target.name]: target.value,
-        });
-    }
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const abortController = new AbortController();
-        const response = await createCard(
-            deckId,
-            { ...newCard },
-            abortController.signal
-        );
-        history.go(0);
-        setNewCard(initialState);
-        return response;
-    }
-
-    async function handleDone() {
-        history.push(`/decks/${deckId}`);
-    }
-
+  
+    // change handler for new card data changes
+    const handleChange = ({ target }) => {
+      setNewCard((currentState) => ({
+        ...currentState,
+        [target.name]: target.value,
+      }));
+    };
+  
+    
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      await createCard(deckId, newCard);
+      setNewCard(initialFormState);
+      history.go(0);
+    };
     return (
         <div>
             <ol className="breadcrumb">
@@ -64,40 +75,7 @@ function AddCard() {
                 </li>
                 <li className="breadcrumb-item active">Add Card</li>
             </ol>
-            <form onSubmit={handleSubmit}>
-                <h2>{deck.name}: Add Card</h2>
-                <div className="form-group">
-                    <label>Front</label>
-                    <textarea
-                        id="front"
-                        name="front"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={newCard.front}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Back</label>
-                    <textarea
-                        id="back"
-                        name="back"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={newCard.back}
-                    />
-                </div>
-                <button
-                    className="btn btn-secondary mx-1"
-                    onClick={() => handleDone()}
-                >
-                    Done
-                </button>
-                <button className="btn btn-primary mx-1" type="submit">
-                    Save
-                </button>
-            </form>
+            <SharedForm changeHandler={handleChange} submitHandler={handleSubmit} deckId={deckId} newCardData={newCard}/>
         </div>
     );
 }
--- src/Layout/Components/EditCard.js
+++ src/Layout/Components/EditCard.js
@@ -1,113 +1,101 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { readCard, readDeck, updateCard } from "../../utils/api/index";
+import SharedForm from "./SharedForm"
 
 function EditCard() {
+    const mountedRef = useRef(false);
     const { deckId, cardId } = useParams();
     const history = useHistory();
-    const initialDeckState = {
-        id: "",
-        name: "",
-        description: "",
-    };
-    const initialCardState = {
-        id: "",
-        front: "",
-        back: "",
-        deckId: "",
-    };
-
-    const [card, setCard] = useState(initialDeckState);
-    const [deck, setDeck] = useState(initialCardState);
-
+  
+    const initialCardState = { id: '', front: '', back: '', deckId: '' };
+    const [deck, setDeck] = useState({
+      name: 'loading...',
+      description: '',
+    });
+    const [editCard, setEditCard] = useState(initialCardState);
+  
     useEffect(() => {
-        async function fetchData() {
-            const abortController = new AbortController();
-            try {
-                const cardResponse = await readCard(
-                    cardId,
-                    abortController.signal
-                );
-                const deckResponse = await readDeck(
-                    deckId,
-                    abortController.signal
-                );
-                setCard(cardResponse);
-                setDeck(deckResponse);
-            } catch (error) {
-                console.error("Something went wrong", error);
-            }
-            return () => {
-                abortController.abort();
-            };
+      mountedRef.current = true;
+      return () => {
+        mountedRef.current = false;
+      };
+    }, []);
+  
+    useEffect(() => {
+      const abortController = new AbortController();
+      async function loadDeck() {
+        try {
+          const loadedDeck = await readDeck(deckId, abortController.signal);
+          if (mountedRef.current) {
+            setDeck(() => loadedDeck);
+          }
+        } catch (error) {
+          if (error.name !== 'AbortError') {
+            throw error;
+          }
         }
-        fetchData();
-    }, [cardId, deckId]);
-
-    function handleChange({ target }) {
-        setCard({
-            ...card,
-            [target.name]: target.value,
-        });
-    }
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const abortController = new AbortController();
-        const response = await updateCard({ ...card }, abortController.signal);
-        history.push(`/decks/${deckId}`);
-        return response;
-    }
-
-    async function handleCancel() {
-        history.push(`/decks/${deckId}`);
-    }
+      }
+  
+      loadDeck();
+      return () => {
+        abortController.abort();
+      };
+    }, [deckId]);
+  
+    useEffect(() => {
+      const abortController = new AbortController();
+      async function loadCard() {
+        try {
+          const loadedCard = await readCard(cardId, abortController.signal);
+          if (mountedRef.current) {
+            setEditCard(() => loadedCard);
+          }
+        } catch (error) {
+          if (error.name !== 'AbortError') {
+            throw error;
+          }
+        }
+      }
+      loadCard();
+      return () => {
+        abortController.abort();
+      };
+    }, [cardId]);
+  
+    const handleChange = ({ target }) => {
+      setEditCard((currentState) => ({
+        ...currentState,
+        [target.name]: target.value,
+      }));
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      await updateCard(editCard);
+      setEditCard(initialCardState);
+      history.push(`/decks/${deckId}`);
+    };
 
     return (
         <div>
-            <ol className="breadcrumb">
-                <li className="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="breadcrumb-item">
-                    <Link to={`/decks/${deckId}`}>{deck.name}</Link>
-                </li>
-                <li className="breadcrumb-item active">Edit Card {cardId}</li>
-            </ol>
-            <form onSubmit={handleSubmit}>
-                <h2>Edit Card</h2>
-                <div className="form-group">
-                    <label>Front</label>
-                    <textarea
-                        id="front"
-                        name="front"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={card.front}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Back</label>
-                    <textarea
-                        id="back"
-                        name="back"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={card.back}
-                    />
-                </div>
-                <button
-                    className="btn btn-secondary mx-1"
-                    onClick={() => handleCancel()}
-                >
-                    Cancel
-                </button>
-                <button className="btn btn-primary mx-1" type="submit">
-                    Save
-                </button>
-            </form>
+            <nav aria-label='breadcrumb'>
+        <ol className='breadcrumb'>
+          <li className='breadcrumb-item'>
+            <Link to='/'>
+              <i className='fas fa-home'></i> Home
+            </Link>
+          </li>
+          <li className='breadcrumb-item'>
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li className='breadcrumb-item active' aria-current='page'>
+            Edit Card {cardId}
+          </li>
+        </ol>
+      </nav>
+      <h1 className='text-center'>Edit Card</h1>
+            <SharedForm changeHandler={handleChange} submitHandler={handleSubmit} deckId={deckId} newCardData={editCard}/>
         </div>
     );
 }
